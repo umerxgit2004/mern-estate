16a3ba6e2c8b06d6905d7b3d7b39ed83a13eefd1
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-12 12:27:04 +0500

    create a middleware to handle rror

--- api/index.js
+++ api/index.js
@@ -21,3 +21,14 @@ app.listen(3000,()=>{
 app.use(express.json())
 app.use('.api/user',userRouter)
 app.use('/api/auth',authRouter)
+
+app.use((error,req,res,next)=>{
+    const statusCode = error.statusCode || 500
+    const message = error.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+
+    })
+})
