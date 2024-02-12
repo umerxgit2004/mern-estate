3c5d483557bb26201b48d3dec7476fe0ca655db1
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-12 15:36:00 +0500

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import { errorHandler } from "../utils/error.js"
 
 export const signup = async (req,res,next) =>{
     const {username,email,password} = req.body
-
+    
     const hashedPassword = bcryptjs.hashSync(password,10)
     const newUser = new User({username,email,password:hashedPassword})
 
