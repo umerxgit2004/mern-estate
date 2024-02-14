d263bc12a4ef7c5bf08edfc093db3693bdb2cf1d
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-14 22:43:38 +0500

    created user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO)
@@ -19,6 +20,7 @@ app.listen(3000,()=>{
     console.log("Server is running on Port 3000")
 })
 app.use(express.json())
+app.use(cookieParser)
 app.use('.api/user',userRouter)
 app.use('/api/auth',authRouter)
 
